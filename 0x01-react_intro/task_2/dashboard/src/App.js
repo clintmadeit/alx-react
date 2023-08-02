5092c0b46df0594b7bb797d293b6090ab796f02b
clintmadeit <clintonbrirdley@gmail.com>
2023-08-02 09:18:26 +0300

    Login input form

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -13,6 +13,13 @@ function App() {
     </div>
     <div className="App-body">
           <p>Login to access the full dashboard</p>
+            <form>
+              <label htmlFor="email">Email: </label>
+              <input type="email" id="email" name="email" />
+              <label htmlFor="password">Password: </label>
+              <input type="password" id="password" name="password" />
+              <button type="submit">OK</button>
+            </form>
 
       </div>
       <div className="App-footer">
