d38920045711a665338b894720a6653026f7442f
clintmadeit <clintonbrirdley@gmail.com>
2023-08-03 09:32:42 +0300

    code rearrangement

---
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App-footer">
+      <p>Copyright 2020 - holberton School</p>
+    </div>
+    </>
+  );
+}
---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,15 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          School dashboard
+        </h1>
+    </div>
+    </>
+  );
+}
