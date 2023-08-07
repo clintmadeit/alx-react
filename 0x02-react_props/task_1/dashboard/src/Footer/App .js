3f664613d5467263e42d319ba4bdbdbe03a7e699
clintmadeit <clintonbrirdley@gmail.com>
2023-08-07 20:25:55 +0300

    initialization

---
+++ 0x02-react_props/task_1/dashboard/src/Footer/App .js
@@ -0,0 +1,5 @@
+import logo from './logo.png';
+import Notifications from './Notifications';
+import Login from './Login';
+import Footer from './Footer';
+import Header from './Header';
---
+++ 0x02-react_props/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,12 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+    </div>
+    </>
+  );
+}
